59059760f7679cedab593b8c3c566afe0e89180d
Shahbaz Hussain <shahbazhussain141044@gmail.com>
2024-08-02 00:27:21 +0500

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Shahbaz Hussain Azure Deployed App</h1>
+      <h1>Affan Luggar ha Chupa INsan ha bc dalla</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
